ff93ab7cac27da8f58f753053f6854823b028ff9
Hommyah <evil.hom26@gmail.com>
2021-12-09 18:09:00 +0300

    routing start

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 import Calculator from "./pages/calculator";
 import List from "./pages/list";
@@ -9,10 +10,21 @@ import {SliderData} from "./pages/slider/sliderData";
 
 function App() {
   return (
-    <div className="App">
-      <Slider slides={SliderData}/>
-    </div>
+      <div className='App'>
+          <BrowserRouter>
+              <Route path='/slider' component={SliderProps}/>
+              <Route path='/calculator' component={Calculator}/>
+              <Route path='/notice' component={Notification}/>
+              <Route path='/list' component={List}/>
+              <Route path='/square' component={Square}/>
+          </BrowserRouter>
+      </div>
+
   );
 }
+
+const SliderProps= () => <div>
+    <Slider slides={SliderData}/>
+</div>
 
 export default App;
